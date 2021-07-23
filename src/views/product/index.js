326e2b50d74de2d9e354a8237a40883bb31437af
green-production <produce.recycle@gmail.com>
2021-07-23 08:48:12 -0700

    test

--- src/views/product/index.js
+++ src/views/product/index.js
@@ -13,7 +13,7 @@ class Product extends Component {
     render() {
         return (
             <div>
-                Products
+                Products page
             </div>
         );
     }
